8655658d6fb9245b628f23dd0cf5073071698985
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-10-18 20:23:37 -0400

    chore: clippy::redundant_pattern_matching
    warning: redundant pattern matching, consider using `is_err()`
       --> src/byte_record.rs:563:20
        |
    563 |             if let Err(_) = simdutf8::basic::from_utf8(field) {
        |             -------^^^^^^------------------------------------ help: try: `if simdutf8::basic::from_utf8(field).is_err()`
        |
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#redundant_pattern_matching
        = note: `#[warn(clippy::redundant_pattern_matching)]` on by default

--- src/byte_record.rs
+++ src/byte_record.rs
@@ -560,7 +560,7 @@ impl ByteRecord {
         // so we return 0 for the valid_up_to index
         #[cfg(not(feature = "simd_utf8_compat"))]
         for (i, field) in self.iter().enumerate() {
-            if let Err(_) = simdutf8::basic::from_utf8(field) {
+            if simdutf8::basic::from_utf8(field).is_err() {
                 return Err(new_utf8_error(i, 0));
             }
         }
